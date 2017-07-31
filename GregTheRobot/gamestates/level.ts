aacecf2e632f65fdaf334287645a2e7285168608
marcelooliveira <mclricardo@gmail.com>
2017-07-31 19:11:00 -0300

    .
    .

--- GregTheRobot/gamestates/level.ts
+++ GregTheRobot/gamestates/level.ts
@@ -106,7 +106,7 @@ abstract class BaseLevel extends Phaser.State {
 
         //  Populate some tiles for our player to start on
 
-        var lines = this.readFile("/assets/maps/Map0" + this.levelNumber + ".txt").split('\n');
+        var lines = this.readFile("GregTheRobot/GregTheRobot/assets/maps/Map0" + this.levelNumber + ".txt").split('\n');
         for (var y = 0; y < lines.length; y++) {
             var line = lines[y];
             var lineArray = new Array(line.length);
